a045918c6777cfd8d427cc6b466134da63d63592
shaul naim <shaul.naim@forescout.com>
2019-01-05 23:59:45 +0200

    ADD UTILS

--- src/App.js
+++ src/App.js
@@ -1,33 +1,54 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import logo from './logo.svg';
+import api from './utils/api'
+import isLocalHost from './utils/isLocalHost'
 import './App.css';
 
 class App extends Component {
-  
-  componentDidMount(){
-    return fetch('/.netlify/functions/shioors-read-all').then((response) => {
-      return response.json()
+
+  componentDidMount() {
+    api.readAll().then((todos) => {
+      if (todos.message === 'unauthorized') {
+        if (isLocalHost()) {
+          alert('FaunaDB key is not unauthorized. Make sure you set it in terminal session where you ran `npm start`. Visit http://bit.ly/set-fauna-key for more info')
+        } else {
+          alert('FaunaDB key is not unauthorized. Verify the key `FAUNADB_SECRET` set in Netlify enviroment variables is correct')
+        }
+        return false
+      }
+
+      console.log('all todos', todos)
+      this.setState({
+        todos: todos
+      })
     })
   }
-  
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return ( <
+      div className = "App" >
+      <
+      header className = "App-header" >
+      <
+      img src = {
+        logo
+      }
+      className = "App-logo"
+      alt = "logo" / >
+      <
+      p >
+      Edit < code > src / App.js < /code> and save to reload. <
+      /p> <
+      a className = "App-link"
+      href = "https://reactjs.org"
+      target = "_blank"
+      rel = "noopener noreferrer" >
+      Learn React FAUNADB NETLIFY SERVERLESS FUNCTIONS  <
+      /a> <
+      /header> <
+      /div>
     );
   }
 }
